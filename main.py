a517b3119f3ffcf7dddc80eea067e9e33ce14343
edeeeen <lsmall@sleepycat.me>
2025-06-12 03:03:08 +0000

    Clean up

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.get("/api")
 def read_root():
-    return {"Hello": "Hello World5"}
+    return {"Hello": "World"}
 
 # first 'static' specify route path, second 'static' specify html files directory.
 app.mount('/', StaticFiles(directory='static',html=True))

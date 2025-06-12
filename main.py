0d017688e25d127fa18fefc097e1e773c08b605e
edeeeen <lsmall@sleepycat.me>
2025-06-12 01:33:58 +0000

    Initial commit from lily

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+#import api
+
+
+
+app = FastAPI()
+
+@app.get("/api")
+def read_root():
+    return {"Hello": "World534564"}
+
+# first 'static' specify route path, second 'static' specify html files directory.
+app.mount('/', StaticFiles(directory='static',html=True))
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=80)

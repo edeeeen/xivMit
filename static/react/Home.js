f619b83b2857165d168f14bf508aa753b290e9d0
edeeeen <lsmall@sleepycat.me>
2025-06-12 03:15:16 +0000

    website test

---
+++ static/react/Home.js
@@ -0,0 +1,17 @@
+"use strict";
+function Home() {
+    return (
+        <div class="home">
+            <h2>Welcome to My Music Reccomendation Page!</h2>
+
+            <p>
+                Here is a website where you can find and submit album or song reccomendations to other users.
+            </p>
+
+            <p id="picture">
+                <img id="mainPic" src="pics/ctftpa750x500.png"></img>
+            </p>
+            
+        </div>
+    );
+}

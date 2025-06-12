dba515d99fb6bfd1146f780642e368b5db03e135
edeeeen <lsmall@sleepycat.me>
2025-06-12 01:37:26 +0000

    test

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ app = FastAPI()
 
 @app.get("/api")
 def read_root():
-    return {"Hello": "World534564"}
+    return {"Hello": "World"}
 
 # first 'static' specify route path, second 'static' specify html files directory.
 app.mount('/', StaticFiles(directory='static',html=True))
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=80)
+    uvicorn.run(app, host='0.0.0.0', port=8000)

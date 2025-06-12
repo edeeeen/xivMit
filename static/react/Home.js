49d5c90975d023e79d32bf5c0bd63e281eb783e0
edeeeen <lsmall@sleepycat.me>
2025-06-12 03:19:40 +0000

    clean up

--- static/react/Home.js
+++ static/react/Home.js
@@ -2,15 +2,9 @@
 function Home() {
     return (
         <div class="home">
-            <h2>Welcome to My Music Reccomendation Page!</h2>
+            <h2>Hello World</h2>
 
-            <p>
-                Here is a website where you can find and submit album or song reccomendations to other users.
-            </p>
 
-            <p id="picture">
-                <img id="mainPic" src="pics/ctftpa750x500.png"></img>
-            </p>
             
         </div>
     );

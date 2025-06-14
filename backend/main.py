cb0aa61e1cb362ad32a5a13ad9a605696754c207
edeeeen <lsmall@sleepycat.me>
2025-06-14 03:06:42 +0000

    test commit

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 
 @app.get("/api")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "1World"}
 
 # returns a list of encounters and their properties
 # sorted newest to oldest

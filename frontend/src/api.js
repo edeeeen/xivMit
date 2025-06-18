e1f689bb8e95d9e21f3b311a37c2ca5e9c3bc58e
edeeeen <lsmall@sleepycat.me>
2025-06-18 08:31:41 -0400

    Fix api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 // Create an instance of axios with the base URL
 
 const api = axios.create({
-  baseURL: "xivmit-backend-a3a8ebhaczh2dqar.eastus-01.azurewebsites.net/api"
+  baseURL: "https://xivmit-backend-a3a8ebhaczh2dqar.eastus-01.azurewebsites.net/api"
 });
 
 // Export the Axios instance
